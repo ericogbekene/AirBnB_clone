47b58bee72dd1db9207d39cddf00e675eeee858d
officialmarvyb <marvellousadeyemi5@gmail.com>
2024-01-11 07:39:06 -0800

    Classes updated

---
+++ models/user.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This defines the User Class"""
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """
+    Defines class for User
+    Attributes:
+        email (string):The User's email.
+        first_name (string):The User's first_name.
+        last_name (string): The User's last_name.
+        password (string): The User's created password.
+    """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
