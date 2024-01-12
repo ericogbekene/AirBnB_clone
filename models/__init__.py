d2a7a4e57086278b40d53ae4dce544b2e3a78a33
Eric Ogbekene <ericogbekene@icloud.com>
2024-01-11 17:42:30 -0800

    fixing circular import error

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,14 @@
 #from models import BaseModel
 from models.engine.file_storage import FileStorage
+"""
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+"""
 
 storage = FileStorage()
 storage.reload()
--- models/review.py
+++ models/review.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """ This defines the Class Review"""
 
-from models.base_module import BaseModel
+from models.base_model import BaseModel
 
 class Review(BaseModel):
-	 """Defines class for Review
+    """Defines class for Review
 
     Attributes:
         place_id (str): The id of the place.
         user_id (str): The id of the user.
         text (str): The review text by the user.
     """
-
     place_id = ""
     user_id = ""
     text = ""
