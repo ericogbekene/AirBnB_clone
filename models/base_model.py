6bbf43860a884de06dbdb94415e73f07f6bd925d
Eric Ogbekene <ericogbekene@icloud.com>
2024-01-13 18:10:07 -0800

    cleaned out code and ensured pycodestyle validation

--- models/base_model.py
+++ models/base_model.py
@@ -19,10 +19,14 @@ class BaseModel:
         """
         Overiding string representation of object to print desired output
         """
-        return "[{}] ({}) {}".format(__class__.__name__, self.id, self.__dict__)
-    
+        return "[{}] ({}) {}"\
+            .format(__class__.__name__, self.id, self.__dict__)
+
     def save(self):
-        """ Public instance method to update attribute updated_at with current datetime"""
+        """
+        method to update attribute -
+        updated_at with current datetime
+        """
         self.updated_at = datetime.datetime.now()
         storage.save()
 
@@ -32,24 +36,30 @@ class BaseModel:
         """
         obj_dict = {k: v for k, v in self.__dict__.items()}
         obj_dict['__class__'] = self.__class__.__name__
-        if hasattr(self, 'created_at') and isinstance(self.created_at, datetime.datetime):
-            obj_dict['created_at'] = self.created_at.isoformat()
-        """ should this use strftime instead ??"""
-        if hasattr(self, 'updated_at') and isinstance(self.created_at, datetime.datetime):
-            obj_dict['updated_at'] = self.updated_at.isoformat()
+
+        if hasattr(self, 'created_at'):
+            created_at = getattr(self, 'created_at')
+            if isinstance(created_at, datetime.datetime):
+                obj_dict['created_at'] = created_at.isoformat()
+
+        if hasattr(self, 'updated_at'):
+            updated_at = getattr(self, 'updated_at')
+            if isinstance(updated_at, datetime.datetime):
+                obj_dict['updated_at'] = updated_at.isoformat()
+
         return obj_dict
-    
+
     def __init__(self, *args, **kwargs):
         """ Generate an instance from a dictionary representation"""
         if kwargs:
             for key, value in kwargs.items():
                 if key not in ['__class__']:
                     if key in ['created_at', 'updated_at']:
-                        setattr(self, key, datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                        setattr(self, key, datetime.datetime.strptime
+                                (value, '%Y-%m-%dT%H:%M:%S.%f'))
                     else:
                         setattr(self, key, value)
         else:
-            #super().__init__(*args)
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             storage.new(self)
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,6 +6,7 @@ import unittest
 import datetime
 from models.base_model import BaseModel
 
+
 class TestBaseModel(unittest.TestCase):
     """
     class to test the functionality of BaseModel
@@ -24,13 +25,13 @@ class TestBaseModel(unittest.TestCase):
         check if created at is datetime object
         """
         self.assertIsInstance(test_model.created_at, datetime.datetime)
-        #check if updated at is datetime obj
+        # check if updated at is datetime obj
         self.assertIsInstance(test_model.updated_at, datetime.datetime)
-        
-        #self.assertIsInstance
-        #test save method
-        #test todict method
-        
+
+        # self.assertIsInstance
+        # test save method
+        # test todict method
+
     def test_save(self):
         """ test that updated at works properly with save"""
         save_model = BaseModel()
@@ -38,21 +39,22 @@ class TestBaseModel(unittest.TestCase):
         save_model.save()
         updated_time = save_model.updated_at
         self.assertNotEqual(initial_time, updated_time)
-        
+
     def test_str(self):
         """
         test that the string representation of the instance prints properly
         """
         new_model = BaseModel()
-        str_test = "[BaseModel] ({}) {}".format(new_model.id, new_model.__dict__)
-        
+        str_test = "[BaseModel] ({}) {}"\
+            .format(new_model.id, new_model.__dict__)
+
         self.assertEqual(str_test, str(new_model))
-        
+
     def test_dict_method(self):
         """
         test that to dict method works properly
         """
-    
+
     def test_kwargs(self):
         """
         test init method of new instance with **kwargs
@@ -63,12 +65,12 @@ class TestBaseModel(unittest.TestCase):
             "updated_at": "2017-09-28T21:05:54.119572",
             "__class__": "BaseModel"
         }
-        
+
         model_from_dict = BaseModel(**test_dict)
-        
+
         self.assertIsInstance(model_from_dict, BaseModel)
         self.assertEqual(model_from_dict.id, test_dict["id"])
-        
-        
+
+
 if __name__ == '__main__':
     unittest.main()
